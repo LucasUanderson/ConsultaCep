364a1c3efbe65d61af406b25cd01a7269a852b15
LucasUanderson <lucasuanderson159@live.com>
2023-03-31 03:50:07 -0300

    iniciando o projeto

---
+++ correiosconsultacep/src/main/java/br/com/correiosconsultacep/CorreiosconsultacepApplication.java
@@ -0,0 +1,13 @@
+package br.com.correiosconsultacep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreiosconsultacepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorreiosconsultacepApplication.class, args);
+	}
+
+}
---
+++ correiosconsultacep/src/test/java/br/com/correiosconsultacep/CorreiosconsultacepApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.correiosconsultacep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreiosconsultacepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
