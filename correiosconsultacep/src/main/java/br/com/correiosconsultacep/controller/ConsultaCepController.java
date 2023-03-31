d8b42c9b17cf18151110a76226fac6a14e560522
LucasUanderson <lucasuanderson159@live.com>
2023-03-31 05:25:43 -0300

    atualizando classe e dependencia swagger

--- correiosconsultacep/src/main/java/br/com/correiosconsultacep/controller/ConsultaCepController.java
+++ correiosconsultacep/src/main/java/br/com/correiosconsultacep/controller/ConsultaCepController.java
@@ -2,6 +2,11 @@ package br.com.correiosconsultacep.controller;
 
 import br.com.correiosconsultacep.model.dtos.Request;
 import br.com.correiosconsultacep.service.interfaceservice.ServicoCalculoFrete;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Consulta do CEP", description = "Endpoint para buscas de CEPs")
 public class ConsultaCepController {
     @Autowired
     private ServicoCalculoFrete service;
@@ -26,6 +32,9 @@ public class ConsultaCepController {
      * @return um objeto ResponseEntity com a resposta da requisição HTTP, contendo os dados do endereço consultado.
      */
     @PostMapping("/consulta-endereco")
+    @Operation(summary = "Busca por cep")
+    @ApiResponse(responseCode = "200",description = "OK", content ={
+            @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseEntity.class))})
     public ResponseEntity consultaCep(@RequestBody Request request) {
         return ResponseEntity.ok(service.executa(request));
     }
