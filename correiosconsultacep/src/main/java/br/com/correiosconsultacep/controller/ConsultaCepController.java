59f0e0e0d265bf2b6b4e846ccb00bdd92fb1acfd
LucasUanderson <lucasuanderson159@live.com>
2023-03-31 04:19:52 -0300

    Classe de controller

--- correiosconsultacep/src/main/java/br/com/correiosconsultacep/controller/ConsultaCepController.java
+++ correiosconsultacep/src/main/java/br/com/correiosconsultacep/controller/ConsultaCepController.java
@@ -1,6 +1,7 @@
 package br.com.correiosconsultacep.controller;
 
 import br.com.correiosconsultacep.model.dtos.Request;
+import br.com.correiosconsultacep.service.interfaceservice.ServicoCalculoFrete;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
